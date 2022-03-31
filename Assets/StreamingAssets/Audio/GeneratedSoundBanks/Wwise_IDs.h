ca508b7dade158ba5e5e5eb37a5886259b7192b0
Nomax <84581437+Nomax-Official@users.noreply.github.com>
2022-03-31 17:26:29 +0200

    Audio Fixed

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,14 +14,25 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID CLOSE_LOCKER = 1966486454U;
+        static const AkUniqueID FIREPLACE_START = 178083583U;
+        static const AkUniqueID FIREPLACE_STOP = 3119796037U;
         static const AkUniqueID FOOTSTEP = 1866025847U;
         static const AkUniqueID GRAB_CLOTHING = 2208075590U;
+        static const AkUniqueID HAMER_STOP = 3370687727U;
+        static const AkUniqueID HAMMER_START = 3650021186U;
+        static const AkUniqueID OPEN_MENU = 2747536169U;
         static const AkUniqueID PLAY_BGM = 3126765036U;
         static const AkUniqueID SEW = 376462276U;
+        static const AkUniqueID START_CUTTING = 1343207550U;
+        static const AkUniqueID START_SEWING = 173461415U;
         static const AkUniqueID STOP_BGM = 1073466678U;
+        static const AkUniqueID TALKING_START = 624453912U;
+        static const AkUniqueID TALKING_STOP = 687379364U;
         static const AkUniqueID UI_CLICK_OPTION = 3276074U;
         static const AkUniqueID UI_SELECT_OPTION = 3364425874U;
         static const AkUniqueID UNSEW = 2002818179U;
+        static const AkUniqueID YOU_LOST = 2017585987U;
+        static const AkUniqueID YOU_WON = 723260527U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
